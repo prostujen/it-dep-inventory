487db39201990eed8e3010b54dd64a1dcdedffde
prostujen <bobrdobrj69@gmail.com>
2026-06-28 08:03:14 +0300

    chore: extend namespaced Adminer\Adminer class for proper compilation

--- adminer-plugins.php
+++ adminer-plugins.php
@@ -1,6 +1,6 @@
 <?php
 // This file is loaded dynamically by Adminer at runtime after the Adminer class is defined.
-class AdminerCustom extends Adminer {
+class AdminerCustom extends Adminer\Adminer {
     function login($login, $password) {
         // Allow empty password database connections for development/demo
         return true;
