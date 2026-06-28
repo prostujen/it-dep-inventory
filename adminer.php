628b4fa46e9a12d37c7f0d99f9932a42675c2309
prostujen <bobrdobrj69@gmail.com>
2026-06-28 14:16:15 +0300

    security: restrict adminer access to logged-in administrators

--- adminer.php
+++ adminer.php
@@ -1,4 +1,10 @@
 <?php
+session_start();
+if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'admin') {
+    http_response_code(403);
+    die("Доступ заборонено. Будь ласка, спочатку авторизуйтеся в системі як адміністратор.");
+}
+
 // Custom Adminer configuration to allow empty password database access
 function adminer_object() {
     require_once './adminer-helper.php';
